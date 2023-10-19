8ce27e8ec05271581036d21be2ce922a826265d1
anujakamble <anujakamble@vctpune.co.in>
2023-10-19 10:44:53 +0530

    added DriverDetails pojo class

---
+++ src/main/java/com/driver/model/DriverDetails.java
@@ -0,0 +1,71 @@
+package com.driver.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="driverDetails")
+public class DriverDetails {
+	@Id
+	@GeneratedValue(strategy =GenerationType.IDENTITY)
+	private int id;
+	private int policyId;
+	private String firstname;
+	private String lastname;
+	private String Dob;
+	private String email;
+	private String mobile;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getPolicyId() {
+		return policyId;
+	}
+	public void setPolicyId(int policyId) {
+		this.policyId = policyId;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getDob() {
+		return Dob;
+	}
+	public void setDob(String dob) {
+		Dob = dob;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getMobile() {
+		return mobile;
+	}
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+	@Override
+	public String toString() {
+		return "DriverDetails [id=" + id + ", policyId=" + policyId + ", firstname=" + firstname + ", lastname="
+				+ lastname + ", Dob=" + Dob + ", email=" + email + ", mobile=" + mobile + "]";
+	}
+	
+	
+
+}
