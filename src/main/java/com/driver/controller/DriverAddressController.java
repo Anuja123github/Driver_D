aa5b803d429afc2d11b8087a42478a111dd8494e
anand <anand.bhunia18@gmail.com>
2023-11-03 13:19:09 +0530

    added eureka client and swagger

--- src/main/java/com/driver/controller/DriverAddressController.java
+++ src/main/java/com/driver/controller/DriverAddressController.java
@@ -13,6 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 import com.driver.model.DriverAddress;
 import com.driver.service.DriverAddressDetails;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 @RestController
 
 public class DriverAddressController {
@@ -20,27 +24,51 @@ public class DriverAddressController {
 	private DriverAddressDetails addressDetails;
 	
 	@PostMapping("/addDriverAddress")
+	@ApiOperation(value = "Request to add address details")
+	@ApiResponses(value = { @ApiResponse(code = 201, message = "Created"),
+			@ApiResponse(code = 400, message = "Invalid Request"),
+			@ApiResponse(code = 500, message = "Internal Error")})
 	public DriverAddress addDetails (@RequestBody DriverAddress driverAddress) {
 		
 		DriverAddress add = addressDetails.addDetails(driverAddress);
 		return add;
 		
 	}
+	
 	@GetMapping("/getDriverAddress/{id}")
+	@ApiOperation(value = "Request to get address details using id")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Ok"),
+			@ApiResponse(code = 404, message = "Resource Not Found"),
+			@ApiResponse(code = 500, message = "Internal Error")})
 	public DriverAddress getDetails (@PathVariable ("Id")Integer id) {
 	DriverAddress get =	addressDetails.getDetails(id);
 	return get;
 	}
+	
 	@PutMapping("/updateDriverAddress")
+	@ApiOperation(value = "Request to update address details using id")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Ok"),
+			@ApiResponse(code = 404, message = "Resource Not Found"),
+			@ApiResponse(code = 500, message = "Internal Error")})
 	public DriverAddress updateDetails  (@RequestBody DriverAddress driverAddress) {
 		DriverAddress update = addressDetails.updateDetails(driverAddress);
 		return update;
 	}
+	
 	@DeleteMapping("/deleteDriverAddress/{id}")
+	@ApiOperation(value = "Request to delete address details using id")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Ok"),
+			@ApiResponse(code = 404, message = "Resource Not Found"),
+			@ApiResponse(code = 500, message = "Internal Error")})
 	public void deleteDetails (@PathVariable ("id") Integer id) {
 		addressDetails.deleteDetails(id);
 	}
+	
 	@GetMapping("/searchDriverByCity/{city}")
+	@ApiOperation(value = "Request to get address details using city")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Ok"),
+			@ApiResponse(code = 404, message = "Resource Not Found"),
+			@ApiResponse(code = 500, message = "Internal Error")})
 	public DriverAddress findByCity (@RequestParam ("city") String city) {
 		DriverAddress search = addressDetails.findByCity(city);
 		return search;
--- src/main/java/com/driver/controller/DriverDetailsController.java
+++ src/main/java/com/driver/controller/DriverDetailsController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,10 @@ import com.driver.dto.DriverDetailsDto;
 import com.driver.model.DriverDetails;
 import com.driver.service.DriverDetailsService;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 @RestController
 public class DriverDetailsController {
 
@@ -25,13 +30,21 @@ public class DriverDetailsController {
 	private DriverDetailsService driverDetailsService;
 
 	@PostMapping("/addDriverDetails")
+	@ApiOperation(value = "Request to save Driver details with address and license details")
+	@ApiResponses(value = { @ApiResponse(code = 201, message = "Created"), 
+			@ApiResponse(code = 400, message = "Invalid Request"),
+			@ApiResponse(code = 500, message = "Internal Error")})
 	public ResponseEntity<DriverDetails> saveDriverDetails(@RequestBody DriverDetails driverDetails) {
 		
 		DriverDetails driverDetails1 = driverDetailsService.saveDriverDetails(driverDetails);
 		return ResponseEntity.ok().body(driverDetails1);
 	}
 	
-	@PostMapping("/updateDriverDetails")
+	@PutMapping("/updateDriverDetails")
+	@ApiOperation(value = "Request to update Driver details with address and license details")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Ok"),
+			@ApiResponse(code = 404, message = "Resource Not Found"),
+			@ApiResponse(code = 500, message = "Internal Error")})
 	public ResponseEntity<DriverDetails> updateDriverDetails(@RequestBody DriverDetails driverDetails) {
 		
 		DriverDetails driverDetails1 = driverDetailsService.updateDriverDetails(driverDetails);
@@ -39,6 +52,10 @@ public class DriverDetailsController {
 	}
 
 	@GetMapping("/getDriverDetails/{id}")
+	@ApiOperation(value = "Request to get Driver details with address and license details using driver id")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Ok"),
+			@ApiResponse(code = 404, message = "Resource Not Found"),
+			@ApiResponse(code = 500, message = "Internal Error")})
 	public ResponseEntity<DriverDetails> getDriverDetails(@PathVariable("id") Integer id) {
 		
 		DriverDetails driverDetails = driverDetailsService.getDriverDetails(id);
@@ -46,6 +63,10 @@ public class DriverDetailsController {
 	}
 
 	@DeleteMapping("/deleteDriverDetails/{id}")
+	@ApiOperation(value = "Request to delete driver details with address and license details using driver id")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Ok"),
+			@ApiResponse(code = 404, message = "Resource Not Found"),
+			@ApiResponse(code = 500, message = "Internal Error")})
 	public ResponseEntity<ApiResponseDto> deleteDriverDetails(@PathVariable("id") Integer id) {
 		
 		driverDetailsService.deleteDriverDetails(id);;
@@ -53,6 +74,10 @@ public class DriverDetailsController {
 	}
 	
 	@GetMapping("/getDriverDetails/byPolicyId")
+	@ApiOperation(value = "Request to get Driver details with address and license details using policy id")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Ok"),
+			@ApiResponse(code = 404, message = "Resource Not Found"),
+			@ApiResponse(code = 500, message = "Internal Error")})
 	public ResponseEntity<DriverDetails> getByPolicyId(@RequestParam("id") Integer policyId) {
 		
 		DriverDetails driverDetails = driverDetailsService.getByPolicyId(policyId);
@@ -60,6 +85,10 @@ public class DriverDetailsController {
 	}
 	
 	@GetMapping("/getDriverDetails/byFirstname")
+	@ApiOperation(value = "Request to get Driver details with address and license details using driver firstname")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Ok"),
+			@ApiResponse(code = 404, message = "Resource Not Found"),
+			@ApiResponse(code = 500, message = "Internal Error")})
 	public ResponseEntity<List<DriverDetails>> getByFirstname(@RequestParam("firstname") String firstname) {
 		
 		List<DriverDetails> driverDetails = driverDetailsService.getByFirstname(firstname);
@@ -67,6 +96,10 @@ public class DriverDetailsController {
 	}
 	
 	@GetMapping("/getDriverDetails/byLastname")
+	@ApiOperation(value = "Request to get Driver details with address and license details using driver lastname")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Ok"),
+			@ApiResponse(code = 404, message = "Resource Not Found"),
+			@ApiResponse(code = 500, message = "Internal Error")})
 	public ResponseEntity<List<DriverDetails>> getByLastname(@RequestParam("lastname") String lastname) {
 		
 		List<DriverDetails> driverDetails = driverDetailsService.getByLastname(lastname);
@@ -74,6 +107,10 @@ public class DriverDetailsController {
 	}
 	
 	@GetMapping("/getDriverDetails/byFirstnameAndLastname")
+	@ApiOperation(value = "Request to get Driver details with address and license details using driver firstname and lastname")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Ok"),
+			@ApiResponse(code = 404, message = "Resource Not Found"),
+			@ApiResponse(code = 500, message = "Internal Error")})
 	public ResponseEntity<List<DriverDetails>> getByFirstnameAndLastname(@RequestParam("firstname") String firstname, @RequestParam("lastname") String lastname) {
 		
 		List<DriverDetails> driverDetails = driverDetailsService.getByFirstnameAndLastname(firstname, lastname);
@@ -81,6 +118,10 @@ public class DriverDetailsController {
 	}
 	
 	@GetMapping("/getDriverDetails/byEmail")
+	@ApiOperation(value = "Request to get Driver details with address and license details using driver email")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Ok"),
+			@ApiResponse(code = 404, message = "Resource Not Found"),
+			@ApiResponse(code = 500, message = "Internal Error")})
 	public ResponseEntity<DriverDetails> getByEmail(@RequestParam("email") String email) {
 		
 		DriverDetails driverDetails = driverDetailsService.getByEmail(email);
@@ -88,6 +129,10 @@ public class DriverDetailsController {
 	}
 	
 	@GetMapping("/getDriverDetails/byMobile")
+	@ApiOperation(value = "Request to get Driver details with address and license details using driver mobile number")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Ok"),
+			@ApiResponse(code = 404, message = "Resource Not Found"),
+			@ApiResponse(code = 500, message = "Internal Error")})
 	public ResponseEntity<DriverDetails> getByMobile(@RequestParam("mobile") String mobile) {
 		
 		DriverDetails driverDetails = driverDetailsService.getByMobile(mobile);
@@ -95,6 +140,10 @@ public class DriverDetailsController {
 	}
 	
 	@GetMapping("/getDriverPolicyDetails/{id}")
+	@ApiOperation(value = "Request to get Driver details with policy details using driver id. This is dependent on Inusrance_D")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Ok"),
+			@ApiResponse(code = 404, message = "Resource Not Found"),
+			@ApiResponse(code = 500, message = "Internal Error")})
 	public ResponseEntity<DriverDetailsDto> getDriverPolicyDetails(@PathVariable("id") Integer id) {
 		
 		DriverDetailsDto detailsDto = driverDetailsService.getDriverPolicyDetails(id);
